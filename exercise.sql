1940e6059d64978a10bf2a9782271ebe123cf002
youp bos <2107418@talnet.nl>
2020-03-04 12:06:58 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,1 +1,7 @@
-INSERT INTO planeten (name) VALUE("Zon"),("Mercurius"),("Venus"),("Aarde"),("Mars");
+INSERT INTO planeten(diameter, afstand_tot_de_zon, massa)
+VALUES
+(1392000, 0, 332.946),
+(4880, 57910000, 0.1),
+(12104,108208930 ,0.9),
+(12.56,149597870 ,1),
+(6794,227936640 ,0.1);
