2a56a5739d5e44f836bbb4b016eebb61ec80d645
youp bos <2107418@talnet.nl>
2020-03-05 14:18:08 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,7 +1,6 @@
-INSERT INTO planeten(diameter, afstand_tot_de_zon, massa)
+-- ALTER TABLE planeten 
+-- ADD (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY);
+
+INSERT INTO pl3aneten(name, diameter, afstand_tot_de_zon, massa, bezoek_datum, id)
 VALUES
-(1392000, 0, 332.946),
-(4880, 57910000, 0.1),
-(12104,108208930 ,0.9),
-(12.56,149597870 ,1),
-(6794,227936640 ,0.1);
+("mars", 6794, 227936640, 0.1, NOW(), 6);
